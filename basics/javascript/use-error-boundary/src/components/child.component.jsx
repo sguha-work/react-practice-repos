c997e05d7e1bbd45c7ea433ef8a6085780805888
sguha-work <sguha1988.life@gmail.com>
2024-04-03 19:25:47 +0530

    custom function

--- basics/javascript/use-error-boundary/src/components/child.component.jsx
+++ basics/javascript/use-error-boundary/src/components/child.component.jsx
@@ -1,16 +1,28 @@
 import { useInterval } from 'usehooks-ts'
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import { useErrorBoundary } from "react-error-boundary";
 function ChildComponent() {
-    const [counter, setCounter] = useState(0);
-    const { showBoundary } = useErrorBoundary();
-    useInterval(() => {
-        setCounter(counter + 1);
-        console.log(counter);
-        if(counter>15) {
+  const [counter, setCounter] = useState(0);
+  const { showBoundary } = useErrorBoundary();
+  // useInterval(() => {
+  //     setCounter(counter + 1);
+  //     console.log(counter);
+  //     if(counter>14) {
+  //       showBoundary(new Error("Wrong"));
+  //     }
+  //   }, 1000);
+  useEffect(() => {
+    window.setInterval(() => {
+      setCounter((previousValueOfCounter) => {
+        console.log(previousValueOfCounter, counter);// both will be 0
+        if (previousValueOfCounter > 14) {
           showBoundary(new Error("Wrong"));
         }
-      }, 1000);
+        return previousValueOfCounter + 1;
+      });
+    }, 1000);
+
+  }, [])
   return (
     <h1>{counter}</h1>
   )
