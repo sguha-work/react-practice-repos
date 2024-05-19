95c772c13e7796ab065905f8c2bc0fbbe90b63c5
sguha-work <sguha1988.life@gmail.com>
2024-05-19 23:49:26 +0530

    event bus service changed

--- basics/javascript/understanding-event-bus/src/services/event-bus.service.js
+++ basics/javascript/understanding-event-bus/src/services/event-bus.service.js
@@ -1,13 +1,21 @@
 const EventBus = {
-    on(event, callback) {
-      document.addEventListener(event, (e) => callback(e.detail));
-    },
-    dispatch(event, data) {
-      document.dispatchEvent(new CustomEvent(event, { detail: data }));
-    },
-    off(event, callback) {
-      document.removeEventListener(event, callback);
-    },
-  };
-  
-  export default EventBus;
+  timer: null,
+  on(event, callback) {
+    document.addEventListener(event, (e) => {
+      if(EventBus.timer!=null) {
+        window.clearTimeout(EventBus.timer)
+      }
+      EventBus.timer = window.setTimeout(()=>{
+        callback(e.detail)  
+      });
+    });
+  },
+  dispatch(event, data) {
+    document.dispatchEvent(new CustomEvent(event, { detail: data }));
+  },
+  off(event, callback) {
+    document.removeEventListener(event, callback);
+  },
+};
+
+export default EventBus;
